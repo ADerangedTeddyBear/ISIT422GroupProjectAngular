dfb238c2f65ca2186ce2a342226cc58123d45b7d
ADerangedTeddyBear <cunhivu@gmail.com>
2022-10-23 10:40:18 -0700

    Added styling testbench page for further styling experiments

---
+++ isit422-angular-project/src/app/styling-testbench/styling-testbench.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-styling-testbench',
+  templateUrl: './styling-testbench.component.html',
+  styleUrls: ['./styling-testbench.component.css']
+})
+export class StylingTestbenchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
