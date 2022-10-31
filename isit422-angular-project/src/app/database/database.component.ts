8b7b2ddc468c0f7f5393ded63b37aabe4d9bbd16
Eric Sergio <ericdsergio87@icloud.com>
2022-10-30 20:09:14 -0700

    db using component test with display

--- isit422-angular-project/src/app/database/database.component.ts
+++ isit422-angular-project/src/app/database/database.component.ts
@@ -12,6 +12,7 @@ export class DatabaseComponent implements OnInit {
   name = 'db';
   $http: any;
   message: any;
+  one: any;
 
   constructor() {
     'ngInject';
@@ -19,7 +20,7 @@ export class DatabaseComponent implements OnInit {
   }
   
   oninsert() {
-    var names = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen',];
+    var names = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     for(var i = 0;i < 20;i++) {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", 'http://localhost:5000/api/insert', true);
@@ -58,49 +59,37 @@ ondelete() {
 ondisplay() {
   var xhr = new XMLHttpRequest();
   var n = 0;
-  this.buildTable();
+  
   xhr.onreadystatechange = () => { 
     if (xhr.readyState == 4 && xhr.status == 200)
-      
-        //console.log(`${JSON.parse(xhr.responseText)}`);
-        //var resObj = xhr.responseText;
         var o = JSON.parse(xhr.responseText);
-        
+        this.buildTable();
         var newRow = document.getElementsByTagName('tr');
         for(var p in o) {
-          n += 1;
+          
           console.log(`${n}: ${o[p]}`)
-          //console.log(JSON.stringify(o[p]))
           var da = (o[p]);
+          this.addRow(da.id, n);
+          this.addData(da.name, n);
+          this.addData(da.tag, n);
+          n += 1;
+          document.getElementsByTagName('tr')[n].removeAttribute('class');
+          //document.querySelector('body > app-root > div > db-database > table > .current')?.removeAttribute('class')
           for(var p in da) {
-            this.addRow(da.id, n);
-            
-            this.addData(da.name, n);
-            this.addData(da.tag, n);
-            document.getElementsByTagName('tr')[n].removeAttribute('class');
+            //document.querySelector('body > app-root > div > db-database > table > .current')?.removeAttribute('class')
           }
-          //this.addRow(JSON.stringify(o[p]));
-
-          //document.getElementsByTagName('tr')[0].classList.remove('current');
-
         }
         var tbl = document.getElementsByClassName('displayTbl');
-        //$('table').css('color', 'red');
         this.rows();
-
+        console.log(`${document.querySelector('body > app-root > div > db-database > table')}`)
 }
-
-
 xhr.open("GET", 'http://localhost:5000/api/display', true);
 xhr.send(/*JSON.stringify({test:"test1"})*/null);
-  
-}
 
+}
 buildTable() {
   var tbl = document.createElement('table');
   var tblRows = document.getElementsByTagName('tr');
-  //var tblRow = document.createElement('tr');
-  //var tbldata = document.createElement('td')
   var divElem = document.getElementsByClassName('dbGrid')[0]
   divElem.after(tbl);
   tbl.setAttribute('class', 'displayTbl');
@@ -108,47 +97,48 @@ buildTable() {
   tbl.style.left = '13%';
   tbl.style.top = '45%';
   tbl.style.backgroundColor = '#00afdd';
+  tbl.style.border = "solid 7px #000000";
   //tbl.style.border = '1';
   //console.log(`${getComputedStyle(tbl).getPropertyValue('border')} --------------------`)
-
 }
 
 addRow(data:any, n:number) {
   var tbl = document.getElementsByClassName('displayTbl')[0];
   var row = document.createElement('tr');
-  
   tbl.appendChild(row);
-  //var newRowCount = newRow.length;
-  //console.log(newRowCount)
-  //var n = document.getElementsByTagName('tr').length;
-  //document.getElementsByTagName('tr')[n].setAttribute('class', 'current');
   row.setAttribute('class', 'current');
-  this.addData(data, n);
-  //document.getElementsByClassName('current')[0].removeAttribute('current')
-  //row.removeAttribute('class')
-  
+  row.style.border = "solid 1px #000000";
+  this.addData(data, n);  
 }
 
 addData(d:any, n:number) {
   //body > app-root > div > db-database > table:nth-child(4) > tr:nth-child(1)
-
   var data = document.createElement('td');
   var curRow = document.getElementsByClassName('current')[n];
   curRow.appendChild(data);  
   var textNode = document.createTextNode(d);
   textNode.appendData(d);
   data.appendChild(textNode);
-  
-  
+  data.style.border = "solid 1px #000000";
+  var tbl = document.getElementsByClassName('displayTbl')[0];
+  //console.log(document.querySelector('body > app-root > div > db-database > table > .current')?.removeAttribute('class'))
+  //console.log(`${getComputedStyle(tbl).getPropertyValue('border')} --------------------`);
 }
 
 rows() {
   var tbl = document.getElementsByClassName('displayTbl')[0];
   var rows = document.getElementsByClassName('row');
+  var tbl = document.getElementsByClassName('displayTbl')[0];
+  //console.log(`${/*tbl.setAttribute('style.border', 'solid 1px rgb(128, 128, 128)')*/}`); 
+  //getComputedStyle(tbl).setProperty('border', 'solid 1px rgb(128, 128, 128)')
+//  console.log(this)
+  //console.log(document.querySelector('body > app-root > div > db-database > table > tr'))
+  
 }
 
-
+//body > app-root > div > db-database > table
+//body > app-root > div > db-database > .displayTbl
   ngOnInit(): void {
+    
   }
-
 }
