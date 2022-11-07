69169aa1a3f5ba7292acf94d80523bc78e719113
Eric Sergio <ericdsergio87@icloud.com>
2022-11-07 01:50:52 -0800

    major db service refactor added findAndModify and updateMany in progress

--- isit422-angular-project/src/app/database/database.component.ts
+++ isit422-angular-project/src/app/database/database.component.ts
@@ -11,44 +11,71 @@ export class DatabaseComponent /*implements OnInit*/ {
   name = 'db';
   one: any;
   FormsService: any;
-  
+  static path: any;
+  static id: number;
 
-  constructor() {
-    'ngInject';
-    ;
-  }
-  
-  oninsert() {
-    //FormsService.postData();
-    FormsService.adminDisplayCollections();
-  }
-  onupdate() {
-   FormsService.postData();
+  querys(n:number) {
+    let path = [
+      'http://localhost:5000/api/collections', 'http://localhost:5000/api/insert',
+     'http://localhost:5000/api/courses', 'http://localhost:5000/api/display', 'http://localhost:5000/api/updateMany',
+     'http://localhost:5000/api/findAndModify'
+    ];
+    return path[n];
   }
 
-  ondelete() {
-    FormsService.adminDisplayData2();
+  dropdown() {
+    document.getElementsByClassName('dropdn')[0].classList.toggle("show");
   }
 
-  ondisplay() {
-    FormsService.adminDisplayData();
+  checkClassExists(idx:number) {
+    let classExists = document.getElementsByClassName('clicked')[0];
+    let setClass = document.getElementsByTagName('button')[idx].setAttribute('class', 'clicked');
+    classExists ? classExists.removeAttribute('class') : setClass;
+    setClass;    
   }
-/*buildTable() {
-}
 
-addRow(data:any, n:number) {
- 
-}
+  onidx0() {    
+    let query_out = this.querys(0);
+    this.checkClassExists(0);
+    FormsService.adminDisplayData(query_out);
+  }
+  onidx1() {
+    let query_out = this.querys(1);
+    //console.log(`${query_out}`)
+    this.checkClassExists(1);
+   FormsService.postData(query_out);
+  }
 
-addData(d:any, n:number) {
+  onidx2() {
+    let query_out = this.querys(2);
+    this.checkClassExists(2);
+    FormsService.adminDisplayData(query_out);
+  }
 
-}
+  onidx3() {
+    let query_out  = this.querys(3);
+    this.checkClassExists(3);
+    FormsService.adminDisplayData(query_out);
+  }
 
-rows() {
+  onidx4() {
+    let query_out  = this.querys(4);
+    this.checkClassExists(4);
+    FormsService.updateMany(query_out);
+    //FormsService.findAndModify(query_out);
+  }
 
-}
-*/
+  onidx5() {
+    let query_out  = this.querys(5);
+    this.checkClassExists(5);
+    //FormsService.updateMany(query_out);
+    FormsService.findAndModify(query_out);
+  }
+
   ngOnInit(): void {
     //FormsService.getCollections();
+    window.onclick = function(e) {      
+      //console.log(e.target);
+    }
   }
 }
