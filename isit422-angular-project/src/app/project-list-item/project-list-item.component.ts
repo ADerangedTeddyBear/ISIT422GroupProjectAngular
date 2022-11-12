588a9c683e17e6c0f5cfc5bb3ea642ba1d05f48d
ADerangedTeddyBear <cunhivu@gmail.com>
2022-11-12 11:01:00 -0800

    Added variables to prooject list item component to prepare for database values

--- isit422-angular-project/src/app/project-list-item/project-list-item.component.ts
+++ isit422-angular-project/src/app/project-list-item/project-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-project-list-item',
@@ -6,9 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list-item.component.css']
 })
 export class ProjectListItemComponent implements OnInit {
-
+  //modify these variables to use database values
+  projectName: string = "Project 1"
+  projectDescription: string = "this is a random description"
+  studentsAssigned: string = "[Students assigned go here]"
   constructor() { }
-
   ngOnInit(): void {
   }
 
