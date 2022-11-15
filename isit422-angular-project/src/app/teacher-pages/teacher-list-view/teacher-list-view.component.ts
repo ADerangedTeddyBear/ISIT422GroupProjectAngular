ae391d98ce839acaf2696fdc8fab4b2f710ea58e
ADerangedTeddyBear <cunhivu@gmail.com>
2022-11-15 14:34:53 -0800

    Early styling of student list view component and addition of database communications with teacher list view component

--- isit422-angular-project/src/app/teacher-pages/teacher-list-view/teacher-list-view.component.ts
+++ isit422-angular-project/src/app/teacher-pages/teacher-list-view/teacher-list-view.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/control-tests/mock-course';
+import { ProjectList } from 'src/app/control-tests/mock-project-list';
 import { AccountCheckService } from '../../control-tests/account-check.service';
+import { ListDisplayService } from '../../services/list-display.service';
 
 
 @Component({
@@ -9,21 +12,47 @@ import { AccountCheckService } from '../../control-tests/account-check.service';
 })
 export class TeacherListViewComponent implements OnInit {
 
+  // teacherAccount: string = "teacher";
+  courses:Course[] = [];
+  projectListNames: ProjectList[] = []; 
   accountCurrent: string = "";
 
-  constructor(private accountCheck: AccountCheckService) { }
+  projectListsApiUrl = 'http://localhost:5000/api/projectlistsnames';
+  coursesApiUrl = 'http://localhost:5000/api/courses';
 
+  constructor(private accountCheck: AccountCheckService, 
+    private listDisplayService: ListDisplayService) { }
 
+    ngAfterViewInit(){
+
+    }
+    
   ngOnInit(): void {
 
     // Account testing code
     this.accountCheck.currentAccountType.subscribe(accountCurrent => this.accountCurrent = accountCurrent);
     this.setAccountType();
+    //Get courses in array
+    this.getCourses();
+
+    //Get project list names array
+    this.getProjectListName();
   }
 
     // Account testing code
   setAccountType(){
     this.accountCheck.updateAccountType("teacher")
   }
+  // Get courses
+  getCourses(): void {
+    this.listDisplayService.getCourses(this.coursesApiUrl)
+    .subscribe(courses => this.courses = courses);
+    //return this.courses;
+  }
 
+  // Get project list names
+  getProjectListName(): void {
+    this.listDisplayService.getProjectListName(this.projectListsApiUrl)
+    .subscribe(projectListNames => this.projectListNames = projectListNames);
+  }
 }
