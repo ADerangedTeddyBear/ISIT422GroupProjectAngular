a09cf3c9f0157eb970297343ee09dcca6dca97f3
ADerangedTeddyBear <cunhivu@gmail.com>
2022-11-30 16:10:56 -0800

    First iteration of sign in button fix

--- isit422-angular-project/src/app/app.component.ts
+++ isit422-angular-project/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountCheckService } from './control-tests/account-check.service';
+import { ListDisplayService } from './services/list-display.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'isit422-angular-project';
+  accountCurrent: string = "";
 
+  constructor(
+    private accountCheck: AccountCheckService) { }
+
+  ngOnInit(): void {
+    this.accountCheck.currentAccountType.subscribe(accountCurrent => this.accountCurrent = accountCurrent);
+    console.log("Account Current: " + this.accountCurrent)
+  }
 }
